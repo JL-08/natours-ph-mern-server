351311887eb520271347ace5f4f74ae9da3b71b8
JL-08 <janellelacsamana@gmail.com>
2021-04-27 22:48:02 +0800

    Add review and tour models | set up for importing data for json file

---
+++ models/tourModel.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+// const slugify = require('slugify');
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxLength: [40, 'A tour name must contain 10 to 40 characters'],
+      minLength: [10, 'A tour name must contain 10 to 40 characters'],
+      validate: [validator.isAlpha, 'A tour name must only contain letters'],
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a maximum group size'],
+    },
+    activities: [
+      {
+        type: String,
+        enum: {
+          values: ['Biking', 'Hiking', 'Swimming', 'Sightseeing'],
+          message:
+            'Activities is either: Biking, Hiking, Swimming, Sightseeing',
+        },
+      },
+    ],
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+      max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          return val < this.price;
+        },
+        message:
+          'Discount price ({VALUE}) should be lower than the regular price',
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: true,
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    mainLocation: {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
